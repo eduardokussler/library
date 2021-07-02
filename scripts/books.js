abc92ec465a2e4e644445e7392ee4d1359e365e3
Eduardo Kussler <eduardoeugeniok@gmail.com>
2021-07-02 15:44:57 -0300

    Remove book functionality added

--- scripts/books.js
+++ scripts/books.js
@@ -11,24 +11,32 @@ let Book = {
   }
 }
 
-function addBookToLibrary(library, book) {
+function addBookToLibrary(book) {
   library.push(book);
-  return library;
 }
 
-function renderLibraryToHtml(library, placeToAppend) {
-  let newRow = {
-    title: document.createElement('td'),
-    author: document.createElement('td'),
-    pages: document.createElement('td'),
-    read: document.createElement('td'),
-  }
+function renderLibraryToHtml(placeToAppend) {
+  
   for(let i = 0; i < library.length; i++) {
+    let newRow = {
+      title: document.createElement('td'),
+      author: document.createElement('td'),
+      pages: document.createElement('td'),
+      read: document.createElement('td'),
+      remove: document.createElement('td'),
+    };
     let newTr = document.createElement('tr');
+    let button = document.createElement('button');
+    button.innerText = 'Remove Book';
     newRow.title.innerText = library[i].title;
     newRow.author.innerText = library[i].author;
     newRow.pages.innerText = library[i].pages;
     newRow.read.innerText = library[i].read;
+    button.setAttribute('index', `${i}`);
+    button.addEventListener('click', (e) => {
+      removeBook(e.target.getAttribute('index'));
+    })
+    newRow.remove.appendChild(button);
     for(col in newRow) {
       newTr.appendChild(newRow[col]);
     }
@@ -37,26 +45,39 @@ function renderLibraryToHtml(library, placeToAppend) {
   
 }
 
+function removeBook(index) {
+  index = Number(index);
+  library.splice(index, 1);
+  clearTable();
+  renderLibraryToHtml(table);
+}
+
 function addBook() {
   let newBookTitle = prompt('Book title: ');
   let newBookAuthor = prompt(`Who's the author? `);
   let newBookPages = prompt('How many pages? ');
   let userHasRead = prompt('Have you read it already? YES/NO');
   userHasRead = userHasRead.toUpperCase();
-  let newBookRead = userHasRead === 'YES'? true:false;
+  let newBookRead = userHasRead === 'YES' ? true:false;
   let newBook = Object.create(Book).init(newBookTitle, newBookAuthor,
                                         newBookPages, newBookRead);
-  library = addBookToLibrary(library, newBook);                                 
-  renderLibraryToHtml(library, tableHeader);
+  addBookToLibrary(newBook); 
+  clearTable();                                
+  renderLibraryToHtml(table);
 }
 
+function clearTable() {
+  let tableHeader = document.querySelector('thead');
+  while(tableHeader.nextSibling) {
+    tableHeader.nextSibling.remove();
+  }
+}
 
 const addBookButton = document.querySelector('#newBook');
 addBookButton.addEventListener('click', () => {
   addBook();
 })
 let library = [];
-const tableHeader = document.querySelector('table');
-library = addBookToLibrary(library, lordOfTheRings);
-renderLibraryToHtml(library, tableHeader);
+const table = document.querySelector('table');
+renderLibraryToHtml(table);
 
